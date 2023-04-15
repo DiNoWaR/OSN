74027c47cd521bee5f36a7b5e9127860bb5c7483
denis.vasilev <denis.vasilev@careem.com>
2023-04-15 20:17:52 +0400

    Finishes third task

--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ from pyspark.sql.functions import *
 
 RAW_CSV_PATH = "/Users/denisvasilyev/Documents/Projects/OSN/input/groceries.csv"
 UNIQUE_PRODUCTS_PATH = "/Users/denisvasilyev/Documents/Projects/OSN/out/out_1_2a.txt"
+TOTAL_COUNTS_PRODUCTS_PATH = "/Users/denisvasilyev/Documents/Projects/OSN/out/out_1_2b.txt"
+TOP_FIVE_PURCHASED_PATH = "/Users/denisvasilyev/Documents/Projects/OSN/out/out/out_1_3.txt"
 
 
 def read_raw_file_and_preprocess(path):
     raw_dataframe = spark.read.text(path)
     return raw_dataframe \
         .select(split('value', ',').alias("product")) \
-        .select(explode('product').alias("product"))
+        .select(explode('product').alias("product")) \
+        .filter(col('product').isNotNull() & (length(col("product")) > 0))
 
 
 def write_dataframe(dataframe, output_path):
@@ -17,7 +20,7 @@ def write_dataframe(dataframe, output_path):
         .write \
         .option("header", "false") \
         .mode("overwrite") \
-        .text(output_path)
+        .csv(output_path)
 
 
 def save_unique_products(dataframe):
@@ -27,6 +30,23 @@ def save_unique_products(dataframe):
     write_dataframe(unique_products, UNIQUE_PRODUCTS_PATH)
 
 
+def save_count_of_all_products(dataframe):
+    counts = dataframe.select(expr("count(*)").alias('total')) \
+        .withColumn("total", concat(lit('Count: '), col('total'))) \
+
+    write_dataframe(counts, TOTAL_COUNTS_PRODUCTS_PATH)
+
+
+def save_top_five(dataframe):
+    top_five = dataframe.groupBy('product') \
+        .agg(count("*").alias('total')) \
+        .orderBy(col('total').desc()) \
+
+    write_dataframe(top_five, TOP_FIVE_PURCHASED_PATH)
+
+
 if __name__ == "__main__":
     dataframe = read_raw_file_and_preprocess(RAW_CSV_PATH)
     save_unique_products(dataframe)
+    save_count_of_all_products(dataframe)
+    save_top_five(dataframe)
